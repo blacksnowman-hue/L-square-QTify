3b62248925992101db234e4717522ed97dcc86da
Harsh <htharshthakur07@gmail.com>
2024-02-27 15:43:00 +0530

    added hero section

--- qtify/src/components/HeroImage/HeroImage.jsx
+++ qtify/src/components/HeroImage/HeroImage.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import './HeroImage.css'
-import HeroLogo from '../../Images/heroLogo'
-
-const HeroImage = () => {
+import HeroImage from '../../Images/vibrating-headphone.png'
+import styles from './HeroImage.module.css'
+const Hero = () => {
   return (
-    <div className='heroimage'>
-        <div>
-        <h1>100 Thousand Songs, ad-free</h1>
-        <h1>Over thousands podcast episodes</h1>
-        </div>
-        
-        <div className='herologo'><HeroLogo /></div>
-    </div>
-  );
+    <>  
+        <section className={styles.Hero}>
+            <div className={styles.HeroTitle}>
+                <h1>100 Thousand Songs, ad-free</h1>
+                <h1>Over thousands podcast episodes</h1>
+            </div>
+                <img className={styles.HeroImage} src={HeroImage} alt='headphone'/>
+        </section>
+    </>
+  )
 }
 
-export default HeroImage
+export default Hero
